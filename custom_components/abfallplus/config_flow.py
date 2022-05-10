fb46eeeec2292279484b73687f1f76384c5151f4
bouni <bouni@owee.de>
2022-05-10 10:49:15 +0000

    almost working config flow

--- custom_components/abfallplus/config_flow.py
+++ custom_components/abfallplus/config_flow.py
@@ -1,13 +1,11 @@
 from homeassistant import config_entries
-from homeassistant.helpers.selector import selector
 import logging
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from .const import DOMAIN
-from .scraper import get_api_key, get_cities
+from .scraper import get_api_key, get_cities, get_districts, get_streets, get_trash_types
 
 _LOGGER = logging.getLogger(__name__)
-
 # https://github.com/home-assistant/example-custom-config/tree/master/custom_components/detailed_hello_world_push
 # https://github.com/boralyl/github-custom-component-tutorial/blob/master/custom_components/github_custom/config_flow.py
 # https://github.com/BenPru/luxtronik/blob/main/custom_components/luxtronik/config_flow.py
@@ -17,7 +15,7 @@ class AbfallPlusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """AbfallPlus config flow."""
 
     async def async_step_user(self, user_input):
-        """ get the municipality waste managemnet website URL from the user"""
+        """get the municipality waste managemnet website URL from the user"""
         if user_input is not None:
             # If the user has entered the URL, scrape the API key from the website and continue
             try:
@@ -25,26 +23,95 @@ class AbfallPlusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception as e:
                 _LOGGER.error(e)
             return await self.async_step_city()
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({vol.Required("URL"): str}))
-
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({vol.Required("URL"): str})
+        )
 
     async def async_step_city(self, user_input=None):
         if user_input is not None:
-            # if the user has selected a city and pressed confirm, go ahead to the next step
             self.city = user_input["CITY"]
-            return await self.async_step_road()
+            return await self.async_step_district()
         else:
-            # if we just got to this step, fetch a list of citys with the API key we got in the initial step
+            self.cities = {}
             try:
-                self.cities = await get_cities(self.api_key)
+                data = await get_cities(self.api_key)
+                self.cities = data.get("cities")
+                self.hidden_name = data.get("hidden_name")
+                self.hidden_value = data.get("hidden_value")
             except Exception as e:
                 _LOGGER.error(e)
-        
-        return self.async_show_form(step_id="city", data_schema=vol.Schema({vol.Required("CITY"): vol.In(self.cities)}))
-    
-    async def async_step_road(self, user_input=None):
+        return self.async_show_form(
+            step_id="city",
+            data_schema=vol.Schema({vol.Required("CITY"): vol.In(self.cities)}),
+        )
 
-        _LOGGER.error(self.city)
+    async def async_step_district(self, user_input=None):
+        if user_input is not None:
+            self.district = user_input["DISTRICT"]
+            return await self.async_step_street()
+        else:
+            self.districts = {}
+            try:
+                self.districts = await get_districts(
+                    self.api_key, self.city, self.hidden_name, self.hidden_value
+                )
+                if len(self.districts) == 1:
+                    self.district = list(self.districts.keys())[0]
+                    return await self.async_step_street()
+            except Exception as e:
+                _LOGGER.error(e)
+        return self.async_show_form(
+            step_id="district",
+            data_schema=vol.Schema({vol.Required("DISTRICT", default=list(self.districts.keys())[0]): vol.In(self.districts)}),
+        )
 
-        return self.async_show_form(step_id="road", data_schema=vol.Schema({vol.Required("ROAD"): str}))
+    async def async_step_street(self, user_input=None):
+        if user_input is not None:
+            self.street = user_input["STREET"]
+            return await self.async_step_trash_type()
+        else:
+            self.streets = {}
+            try:
+                self.streets = await get_streets(
+                    self.api_key,
+                    self.city,
+                    self.district,
+                    self.hidden_name,
+                    self.hidden_value,
+                )
+                if len(self.streets) == 1:
+                    self.street = list(self.streets.keys())[0]
+                    return await self.async_step_trash_type()
+            except Exception as e:
+                _LOGGER.error(e)
+
+        return self.async_show_form(
+            step_id="street",
+            data_schema=vol.Schema({vol.Required("STREET", default=list(self.streets.keys())[0]): vol.In(self.streets)}),
+        )
+
+    async def async_step_trash_type(self, user_input=None):
+        _LOGGER.warning("Step trash type") 
+        if user_input is not None:
+            self.trash_type = user_input["TRASH_TYPE"]
+        else:
+            self.trash_types = {}
+            try:
+                self.trash_types = await get_trash_types(
+                    self.api_key,
+                    self.city,
+                    self.district,
+                    self.street,
+                    self.hidden_name,
+                    self.hidden_value,
+                )
+                _LOGGER.error(self.trash_types)
+                
+            except Exception as e:
+                _LOGGER.error(e)
+
+        return self.async_show_form(
+            step_id="trash_type",
+            data_schema=vol.Schema({vol.Required("TRASH_TYPE", default=list(self.trash_types.keys())): cv.multi_select(self.trash_types)})
+        )
 
--- custom_components/abfallplus/scraper.py
+++ custom_components/abfallplus/scraper.py
@@ -7,38 +7,105 @@ import aiohttp
 _LOGGER = logging.getLogger(__name__)
 
 
-async def get_api_key(url: str) -> str | None:
+async def get_api_key(url: str) -> str:
     """Scrape the API key from the supplied URL"""
     async with aiohttp.ClientSession() as session:
-        _LOGGER.error(url)
         async with session.get(url) as r:
             page = await r.text()
-            match = re.search(r"https://api.abfall.io/\?key=([a-f0-9]{32})", page)
-            if match:
-                return match.group(1)
+            api_key = re.search(r"https://api.abfall.io/\?key=([a-f0-9]{32})", page)
+            if api_key:
+                return api_key.group(1)
+            return ""
 
 
-async def get_cities(api_key: str) -> dict | None:
-    """Get the list of cities from the API (which is not a real API  but a HTML widget) """
+async def get_cities(api_key: str) -> dict:
+    """Get the list of cities from the API (which is not a real API  but a HTML widget)"""
     async with aiohttp.ClientSession() as session:
         # modus = md5 hash of "scripts"
         url = f"https://api.abfall.io/?key={api_key}&modus=d6c5855a62cf32a4dadbc2831f0f295f&waction=init"
-        _LOGGER.error(url)
         async with session.get(url) as r:
             page = await r.text()
-            cities = {}
+            hidden = re.search(
+                r"name=\"([a-f0-9]{32})\" value=\"([a-f0-9]{32})\"", page
+            )
+            data = {
+                "cities": {},
+                "hidden_name": hidden.group(1),
+                "hidden_value": hidden.group(2),
+            }
             for match in re.findall(
-                r"<option value=\"(\d+)\">([^<]*)</option>", page
+                r"<option value=\"([\d,]+)\">([^<]*)</option>", page
             ):
-                if match[0] != '0':
-                 cities[match[0]] = match[1]
-            return cities
+                if match[0] != "0":
+                    data["cities"][match[0]] = match[1]
+            return data
 
-async def get_district() -> dict | None:
-    pass
 
-async def get_streets() -> dict | None: 
-    pass
+async def get_districts(
+    api_key: str, city_id: str, hidden_name: str, hidden_value: str
+) -> dict:
+    """Get districts for a certain city_id"""
+    async with aiohttp.ClientSession() as session:
+        payload = {"f_id_kommune": city_id, hidden_name: hidden_value}
+        url = f"https://api.abfall.io/?key={api_key}&modus=d6c5855a62cf32a4dadbc2831f0f295f&waction=auswahl_kommune_set"
+        async with session.post(url, data=payload) as r:
+            page = await r.text()
+            districts = {}
+            for match in re.findall(
+                r"<option value=\"([\d,]+)\">([^<]*)</option>", page
+            ):
+                if match[0] != "0":
+                    districts[match[0]] = match[1]
+            return districts
+
+
+async def get_streets(
+    api_key: str, city_id: str, district_id: str, hidden_name: str, hidden_value: str
+) -> dict:
+    """Get streets for a certain city_id and district_id"""
+    async with aiohttp.ClientSession() as session:
+        data = {
+            "f_id_kommune": city_id,
+            "f_id_bezirk": district_id,
+            hidden_name: hidden_value,
+        }
+        url = f"https://api.abfall.io/?key={api_key}&modus=d6c5855a62cf32a4dadbc2831f0f295f&waction=auswahl_bezirk_set"
+        async with session.post(url, data=data) as r:
+            page = await r.text()
+            streets = {}
+            for match in re.findall(
+                r"<option value=\"([\d,]+)\">([^<]*)</option>", page
+            ):
+                if match[0] != "0":
+                    streets[match[0]] = match[1]
+            return streets
+
+
+async def get_trash_types(
+    api_key: str,
+    city_id: str,
+    district_id: str,
+    street_id: str,
+    hidden_name: str,
+    hidden_value: str,
+) -> dict:
+    """Get trash types for a certain city_id, district_id, and street_id"""
+    async with aiohttp.ClientSession() as session:
+        data = {
+            "f_id_kommune": city_id,
+            "f_id_bezirk": district_id,
+            "f_id_strasse": street_id,
+            hidden_name: hidden_value,
+        }
+        url = f"https://api.abfall.io/?key={api_key}&modus=d6c5855a62cf32a4dadbc2831f0f295f&waction=auswahl_strasse_set"
+        async with session.post(url, data=data) as r:
+            page = await r.text()
+            trash_types = {}
+            trash_names = re.findall(r"Abfallart <\/span>([^<]+)*", page)
+            trash_ids = re.findall(r"name=\"f_id_abfalltyp_\d+\" value=\"(\d+)\"", page)
+            trash_types = dict(zip(trash_ids, trash_names))
+            return trash_types
+
 
 #  s = requests.Session()
 #  s.headers.update(
