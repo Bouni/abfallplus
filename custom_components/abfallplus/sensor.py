dadffa752032edad655513fdca2717a19a970635
Linus Dietz <Linus.Dietz@tribus-it.de>
2023-03-28 13:51:11 +0200

    Fix python 3.11 async coroutine

--- custom_components/abfallplus/sensor.py
+++ custom_components/abfallplus/sensor.py
@@ -58,8 +58,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-@asyncio.coroutine
-def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up date sensor."""
     key = config.get(CONF_KEY)
     municipality = config.get(CONF_MUNICIPALITY_ID)
